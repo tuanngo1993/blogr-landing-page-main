6ca810a328f0dd842f95bb48629dffd419629347
Tuan Ngo <nfq@NFQs-MacBook-Pro.local>
2021-05-12 17:52:45 +0700

    first landing page

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        backgroundPosition: {
+            bottom: 'bottom',
+            center: 'center',
+            left: 'left',
+            'left-300': '-300px',
+            right: 'right',
+            top: 'top',
+        },
+        borderRadius: {
+            'none': '0',
+            DEFAULT: '0.25rem',
+            'md': '0.375rem',
+            'lg': '0.5rem',
+            'full': '9999px',
+            '14xl': '7rem' 
+        }
+    }
+}
